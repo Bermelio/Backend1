48db98f496a735afcd229c9194419101e4be2829
Bermelio <facundoperalta258@gmail.com>
2025-05-02 15:22:21 -0300

    por id

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -14,4 +14,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:pid', async (req, res) => {
+    try {
+      const productsID = parseInt(req.params.pid);
+      const filePath = path.join(__dirname, '..', 'data', 'products.json');
+      const data = await fs.readFile(filePath, 'utf-8');
+      const productos = JSON.parse(data);
+      const product = productos.find((p) => p.id === productsID);
+  
+      if (!product) {
+        return res.status(404).json({ error: 'Producto no encontrado 😢' });
+      }
+  
+      res.json(product);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al leer el producto 😢' });
+    }
+  });
+
 module.exports = router;
